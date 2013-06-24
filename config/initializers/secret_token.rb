c45fc2e1a900933b186a992d0b3e24bfd6b8d87e
Mike Mullen <m@m-mullen.com>
2013-06-24 14:27:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pairpal::Application.config.secret_token = '4734757aca0d8bba7af74c1d7d0ae2c9905df6d8601a0bd72caf26b9798e35ef472cbd199c4f8a500df395d2ab9905479e66f698170bd1b76afcb10e9780496e'
